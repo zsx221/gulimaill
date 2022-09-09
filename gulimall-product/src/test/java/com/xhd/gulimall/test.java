3c83c8c7e1c9acc63fdcfd40f61e857d82865e8a
xhd <1365821818@qq.com>
2022-09-09 18:07:07 +0800

    test更新

--- gulimall-product/src/test/java/com/xhd/gulimall/test.java
+++ gulimall-product/src/test/java/com/xhd/gulimall/test.java
@@ -9,11 +9,12 @@ import java.util.UUID;
 import java.util.concurrent.*;
 
 public class test {
-
+    public static ExecutorService executor = Executors.newFixedThreadPool(10);
     @Autowired
     StringRedisTemplate stringRedisTemplate;
+
     @Test
-   public void redisTest() throws ExecutionException, InterruptedException {
+    public void redisTest() throws ExecutionException, InterruptedException {
 //        ValueOperations<String,String> ops = stringRedisTemplate.opsForValue();
 //        //保存
 //        ops.set("hello","word"+ UUID.randomUUID().toString());
@@ -24,36 +25,44 @@ public class test {
         System.out.println("HHHHH");
 //        Thread2 thread2=new Thread2();//继承Thread实现异步
 //        new Thread(thread2).start();
-        FutureTask futureTask=new FutureTask<>(new  Thread3());
+        FutureTask futureTask = new FutureTask<>(new Thread3());
         new Thread(futureTask).start();
         Object o = futureTask.get();
         System.out.println("HHHHH"+ o);
-        ThreadPoolExecutor threadPoolExecutor=new ThreadPoolExecutor(
-                2,
-                50,
-                50,
-                TimeUnit.SECONDS,
-                null,
-                null,
-                null);
+//        ThreadPoolExecutor threadPoolExecutor=new ThreadPoolExecutor(
+//                2,
+//                50,
+//                50,
+//                TimeUnit.SECONDS,
+//                null,
+//                null,
+//                null);
     }
-    class Thread1 implements Runnable{
-        @Override
-        public void run() {
-            System.out.println("现在的线程是"+Thread.currentThread().getName());
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("当前线程:" + Thread.currentThread().getName());
+            int i = 10 / 2;
+            System.out.println("运行结构" + i);
+            return i;
+        }, executor).whenComplete((res, exception) -> {
+            System.out.println("异步完成了...结果是" + res + "异常是:" + exception);
+        });
+        class Thread1 implements Runnable {
+            @Override
+            public void run() {
+                System.out.println("现在的线程是" + Thread.currentThread().getName());
+            }
+        }
+        class Thread2 extends Thread {
+            @Override
+            public void run() {
+                System.out.println("现在的线程是" + Thread.currentThread().getName());
+            }
+        }
+        class Thread3 implements Callable {
+            @Override
+            public String call() throws Exception {
+                System.out.println("现在的线程是" + Thread.currentThread().getName());
+                return "现在的线程是" + Thread.currentThread().getName();
+            }
         }
     }
-    class Thread2 extends Thread{
-        @Override
-        public void run() {
-            System.out.println("现在的线程是"+Thread.currentThread().getName());
-        }
-    }
-    class Thread3 implements Callable {
-        @Override
-        public String call() throws Exception {
-            System.out.println("现在的线程是"+Thread.currentThread().getName());
-            return "现在的线程是"+Thread.currentThread().getName();
-        }
-    }
-}
